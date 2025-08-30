c85636670341656676a15eefa6b798127cdc7687
Zuzim <ajozuzim@gmail.com>
2025-08-30 22:54:22 +0800

    designing Login Form

--- frontend/src/pages/Auth.tsx
+++ frontend/src/pages/Auth.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import type React from 'react'
+import { AuthSection, Container, MainContainer } from '../styles/Container.styled'
+import { TypographyText } from '../components';
+import { Form, Input } from '../styles/Auth.styled';
 
-const Auth = () => {
+
+export const Auth: React.FC = ()=> {
+
+return (
+  <MainContainer>
+    <AuthSection>
+      <LoginContainer/>
+    </AuthSection>
+  </MainContainer>
+)
+}
+
+
+
+ const LoginContainer: React.FC = ()=>{
+  
   return (
-    <div>Auth</div>
+    <Container width='calc(28vw)' height='60vh' justifyContent='center' alignItems='center' backgroundColorLight='#f5f5f5' backgroundColorDark='#333' borderRadius='10px' border='1px solid #ccc' >
+      <LoginWrapper/>
+    </Container>
   )
 }
 
-export default Auth
+const LoginWrapper : React.FC = ()=>{
+  return (
+    <Container width='100%' height='100%' padding='20px' >
+      <TypographyText variant='h5' text='Login' textAlign='center'fontSize="2rem" />
+      <Container border='1px solid red' height='auto' padding='var(--padding-sm)'> 
+        <LoginForm />
+      </Container>
+    </Container>
+  )
+}
+
+const LoginForm: React.FC = ()=>{
+  return (
+    <Form>
+      <Input size='small' label='Email' name='email' />
+      <Input size='small' label='Password' name='password'/>
+    </Form>
+  )
+}
