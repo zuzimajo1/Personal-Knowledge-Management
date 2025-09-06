3ac98141ac9ec31ddd0bef49b39833753bb9bb3b
Zuzim <ajozuzim@gmail.com>
2025-09-06 23:22:00 +0800

    searchbar style

---
+++ frontend/src/styles/SearchBar.styled.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { alpha, InputBase, styled } from '@mui/material'
+
+
+
+
+export const SearchBarContainer = styled('div')(({ theme })=>({
+    display: 'flex',
+    alignItems: 'center',
+    height: '100%',
+    width: '100%',
+    border: '1px solid black',
+    borderRadius: '8px',
+    position: 'absolute',
+}));
+
+
+
+export const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
+
+export const SearchComponent = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  marginRight: theme.spacing(2),
+  width: '100%',
+
+
+   '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
