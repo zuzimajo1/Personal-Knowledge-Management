3e3f50e5c681fefc95e50396d5613d0e5449503a
Zuzim <ajozuzim@gmail.com>
2025-09-27 23:15:56 +0800

    updates

--- frontend/src/components/Notes/Notes.tsx
+++ frontend/src/components/Notes/Notes.tsx
@@ -1,8 +1,67 @@
 import React from 'react'
+import { NotesStyled } from '../../styles/Notes.styled'
+import { TypographyText } from '../Text/TypographyText';
 
 const Notes = () => {
+
+
+interface Note {
+    id: string;
+    title: string;
+    content: string;
+    createdAt: string;
+    updatedAt: string;
+    sharedWith: string[];
+
+}
+
+const notes: Note[] = [
+  {
+    id: '1',
+    title: 'First Notes',
+    content: "This is my first note.",
+    createdAt: '2023-10-01T10:00:00Z',
+    updatedAt: '2023-10-01T10:00:00Z',
+    sharedWith: ['user2', 'user3'],
+  },
+  {
+    id: '2',
+    title: 'Second Note',
+    content: "This is my second note.",
+    createdAt: '2023-10-02T11:00:00Z',
+    updatedAt: '2023-10-02T11:00:00Z',
+    sharedWith: ['user4'],
+  },
+  {
+    id: '3',
+    title: 'Third Note',
+    content: "This is my third note.",
+    createdAt: '2023-10-03T12:00:00Z',
+    updatedAt: '2023-10-03T12:00:00Z',
+    sharedWith: ['user5'],
+
+  },
+  {
+    id: '4',
+    title: 'Fourth Note',
+    content: "This is my fourth note.",
+    createdAt: '2023-10-04T13:00:00Z',
+    updatedAt: '2023-10-04T13:00:00Z',
+    sharedWith: ['user6'],
+  }
+]
+
+
   return (
-    <div>Notes</div>
+    <>
+    {notes.map((note)=>(
+      <NotesStyled key={note.id}>
+        <p>{note.title}</p>
+        <p>{note.content}</p>
+        <p>{`Created at ${note.createdAt}`}</p>
+      </NotesStyled>
+    ))}
+    </>
   )
 }
 
---
+++ frontend/src/styles/Notes.styled.tsx
@@ -0,0 +1,7 @@
+import { styled } from "@mui/material";
+
+export const NotesStyled = styled('div')(({theme})=>({
+    width: 'auto',
+    height: 'auto',
+    border: `1px solid red`,
+}));
