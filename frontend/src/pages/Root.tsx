8647be250709c3cf7177d5414141483978505477
Zuzim <ajozuzim@gmail.com>
2025-09-28 23:05:11 +0800

    updating root

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -28,7 +28,9 @@ const Home: React.FC = () => {
     <ContentContainer>
       <TypographyText text='Welcome to Insight Journal' textAlign='center' fontSize='2rem' fontWeight='600' margin='var(--margin-lg) 0 0 0' />
       <TypographyText text='Your personal space to capture thoughts, ideas, and reflections.' textAlign='center' fontSize='1.2rem' fontWeight='400' margin='var(--margin-sm) 0 0 0' />  
+      <Container width='100%' margin='2rem 0 3rem 0' padding='0 3rem' style={{overflowY: 'auto'}} >
       <Notes />
+      </Container>
     </ContentContainer>
   )
 }
