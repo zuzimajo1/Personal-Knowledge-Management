d69b91414e7c457ffb4eb5f256b58f414f12db9a
Zuzim <ajozuzim@gmail.com>
2025-09-30 22:40:33 +0800

    update

--- frontend/src/styles/Notes.styled.tsx
+++ frontend/src/styles/Notes.styled.tsx
@@ -7,16 +7,15 @@ export const NotesStyled = styled('div')(({theme})=>({
     backgroundColor: theme.palette.mode === 'light' ? '#f8f1f1ff' : '#2c2c2c',
     margin: '1rem',
     padding: '1rem',
-    borderRadius: '4px',
+    borderRadius: '8px',
     boxShadow: theme.palette.mode === 'light' ? "0 2px 4px rgba(0,0,0,0.1)" : "0 2px 4px rgba(255,255,255,0.1)",
 
     '&:hover::-webkit-scrollbar': {
         display: "block",
         transform: "scale(1.2)",
         transition: "transform 0.2s",
+        borderRadius: '8px',
     },
-
-    
     '&::-webkit-scrollbar': {
         width: '12px',
         display: "none",
