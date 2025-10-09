257b199559f19706e519a61c41dd1677d7f43843
Zuzim <ajozuzim@gmail.com>
2025-10-09 23:32:10 +0800

    design shared page

--- frontend/src/pages/Shared.tsx
+++ frontend/src/pages/Shared.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Container, ContentContainer, HomeContainer } from '../styles'
+import { Navbar, Notes, SideBar, TypographyText } from '../components'
+import { ContentComponent } from './Root'
 
 const Shared = () => {
   return (
-    <div>Shared</div>
+    <HomeContainer>
+      <Navbar/>
+        <ContentComponent>
+          <SideBar/>
+          <SharedContent/>
+      </ContentComponent>
+    </HomeContainer>
+  )
+}
+
+
+const SharedContent: React.FC = ()=> {
+  return (
+    <ContentContainer>
+      <TypographyText text='Shared To You' textAlign='center' fontSize='2rem' fontWeight='600' margin='var(--margin-lg) 0 0 0' />
+        <Container width='100%' margin='2rem 0 3rem 0' padding='0 2rem' style={{overflow: "hidden"}} >
+          <Notes />
+        </Container>
+    </ContentContainer>
   )
 }
 
