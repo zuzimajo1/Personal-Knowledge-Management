df905d44dbef88186cbe451331d7d49679135048
Zuzim <ajozuzim@gmail.com>
2025-10-12 23:52:03 +0800

    updates on trash page

--- frontend/src/pages/Trash.tsx
+++ frontend/src/pages/Trash.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Container, ContentContainer, HomeContainer } from '../styles'
+import { Navbar, Notes, SideBar, TypographyText } from '../components'
+import { ContentComponent } from './Root'
 
 const Trash = () => {
   return (
-    <div>Trash</div>
+     <HomeContainer>
+      <Navbar/>
+        <ContentComponent>
+        <SideBar/>
+        <TrashContent/>
+      </ContentComponent>
+    </HomeContainer>
+  )
+}
+
+const TrashContent: React.FC = ()=> {
+  return (
+        <ContentContainer>
+          <TypographyText text='Archive' textAlign='center' fontSize='2rem' fontWeight='600' margin='var(--margin-lg) 0 0 0' />
+            <Container width='100%' margin='2rem 0 3rem 0' padding='0 2rem' style={{overflow: "hidden"}} >
+            <Notes />
+            </Container>
+        </ContentContainer>
   )
 }
 
