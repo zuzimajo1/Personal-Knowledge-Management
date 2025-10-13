8fc959090d653761327d3538d1774e0a7927f19e
Zuzim <ajozuzim@gmail.com>
2025-10-13 23:19:05 +0800

    add defaults

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,3 +3,5 @@ export { Root, ContentComponent } from "./Root";
 export { default as Archive } from "./Archive"
 export { default as Shared } from "./Shared"
 export { default as Trash } from "./Trash"
+export { default as Home } from "./Home"
+export { default as Register } from "./Register"
