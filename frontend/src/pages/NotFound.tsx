62b3160bc18918e645ab5a9808b99f7c18b96c80
Zuzim <ajozuzim@gmail.com>
2025-10-13 23:37:46 +0800

    designing not found page

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { ContentContainer } from '../styles'
+import { TypographyText } from '../components'
 
-const NotFound = () => {
+const NotFound: React.FC = () => {
   return (
-    <div>NotFound</div>
+    <ContentContainer>
+        <TypographyText text='Not Found 434' textAlign='center' fontSize='2rem' fontWeight='600' margin='var(--margin-lg) 0 0 0' />
+    </ContentContainer>
   )
 }
 
