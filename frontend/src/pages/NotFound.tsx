991c52792e8fe3ad4140d7f1fc72427205db3c4b
Zuzim <ajozuzim@gmail.com>
2025-10-15 23:52:20 +0800

    update on not found page

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -5,8 +5,8 @@ import { TypographyText } from '../components'
 const NotFound: React.FC = () => {
   return (
     <HomeContainer>
-      <Container display='flex' alignItems='center' justifyContent='center'>
-        <TypographyText text='Not Found 434' textAlign='center' fontSize='2rem' fontWeight='600' margin='var(--margin-lg) 0 0 0' />
+      <Container height="100vh" display='flex' flexDirection='column'  alignItems='center' justifyContent='center'>
+        <TypographyText text='Sorry! We could not find that page' textAlign='center' fontSize='3rem' fontWeight='600' margin='var(--margin-lg) 0 0 0' />
       </Container>
     </HomeContainer>
   )
