ca152dbdecfc38c7197b27d1f41a60984fe53e23
Zuzim <ajozuzim@gmail.com>
2025-10-16 23:40:32 +0800

    designing register page

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,9 +1,51 @@
 import React from 'react'
+import { AuthSection, Container, Form, Input, MainContainer } from '../styles'
+import { ButtonComponent, ThemeToggle, TypographyText } from '../components'
 
 const Register: React.FC = () => {
   return (
-    <div>Register</div>
+  <MainContainer>
+    <AuthSection>
+      <RegisterContainer/>
+    </AuthSection>
+  </MainContainer>
   )
 }
+
+
+const RegisterContainer: React.FC = ()=>{
+  return (
+    <Container width='calc(20vw)' height='auto' justifyContent='center' alignItems='center' backgroundColorLight='#f5f5f5' backgroundColorDark='#4d4a4aff' borderRadius='10px' border='1px solid #ccc' >
+      <RegisterWrapper/>
+    </Container>
+  )
+}
+
+const RegisterWrapper : React.FC = ()=>{
+  return (
+    <Container width='100%' height='100%' padding='20px 30px' >
+      <TypographyText variant='h5' fontWeight='600' text='Register' textAlign='center'fontSize="2rem" margin='var(--margin-lg) 0' />
+        <RegisterForm/>
+        <ButtonComponent width='100%' text='register' textTransform='uppercase' borderRadius='2rem' backgroundColorLight='#79AFE7' margin='var(--margin-sm) 0' />
+        <Container display='flex' flexDirection='row' justifyContent='center' width='100%' height='auto' margin='var(--margin-lg) 0 0 0'>
+        <ThemeToggle/>
+      </Container>
+    </Container>
+  )
+}
+
+const RegisterForm: React.FC = ()=> {
+  return (
+    <Form>
+      <Input variant="standard" size='small' label='Firstname' name='firstname' />
+      <Input variant="standard" size='small' label='Lastname' name='lastname'/>
+      <Input variant="standard" size='small' label='Email' name='email' />
+      <Input type='password' variant="standard" size='small' label='Password' name='password'/>
+      <Input type='password' variant="standard" size='small' label='Confirm Password' name='confirmpassword'/>
+    </Form>
+  )
+}
+
+
 
 export default Register
